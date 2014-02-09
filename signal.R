7cd5634c5fa16184c6d9f611ad11a9c0413884be
Daniel Krizian <daniel.krizian@gmail.com>
2014-02-09 18:29:45 +0000

    Delete duplicit signal.R

--- signal.R
+++
@@ -1,9 +0,0 @@
-AddColumn(Buy)
-AddColumn(Sell)
-AddColumn(Short)
-AddColumn(Cover)
-
-R[,Sell:=ExRem(Sell, Buy), by=Instrument]
-R[,Buy:=ExRem(Buy,(Sell|Short)), by=Instrument]
-R[,Cover:=ExRem(Cover,Short), by=Instrument]
-R[,Short:=ExRem(Short,(Cover|Buy)), by=Instrument]
