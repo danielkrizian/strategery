c4995f5a236366797e6488771b43003ce84c875e
Daniel Krizian <daniel.krizian@gmail.com>
2014-06-15 00:07:43 +0100

    ATS.R won't collate

--- R/ATS.R
+++ R/ATS.R
@@ -16,37 +16,49 @@ STATES  = factor(1:2, levels=a,labels=a,ordered = F)
 #  closedposition       txn           incash  portfolio$addTxn                 ?
 #
 ############################## 
-while eventPending(){
-  event = getNextEvent()
-  entry = transitions[currentState][event]
-  entry.executeAction()
-  currentState = entry.nextState()
-}
+# components
+################
+# DataHandler -> MarketEvent -> Strategy -> SignalEvent -> Portfolio -> 
+#                                                |
+# 
+# SignalEvent = list(Symbol, Direction=c("Long", "Short"), timestamp)
+# Portfolio = OrderManagementSystem (OMS) + RiskManagement
+#    = c("Risk Management"=c("Exposure", "Position Sizing")) 
 
 
-current_state = STATE_RESET;
-event = EVENT_NONE;
-state_tbl_indx = 0;
-while (1)
-{
-  if (event != EVENT_NONE)
-  {
-    for (j=0; j<max_state_tbl_len; j++)
-    {
-      if (state_tbl[j].cur_state == current_state &&
-            state_tbl[j].event == event)
-      {
-        tmp_event = event;
-        event = EVENT_NONE;
-        (*state_tbl[j].transition_func)(
-          current_state,
-          state_tbl[j].new_state,
-          tmp_event);
-        current_state = state_tbl[j].new_state;
-        state_tbl_indx = j;
-        break;
-      }
-    }
-  }
-  (*state_tbl[state_tbl_indx].monitor_func)(current_state);
-}
+##########
+
+# while eventPending(){
+#   event = getNextEvent()
+#   entry = transitions[currentState][event]
+#   entry.executeAction()
+#   currentState = entry.nextState()
+# }
+# 
+# 
+# current_state = STATE_RESET;
+# event = EVENT_NONE;
+# state_tbl_indx = 0;
+# while (1)
+# {
+#   if (event != EVENT_NONE)
+#   {
+#     for (j=0; j<max_state_tbl_len; j++)
+#     {
+#       if (state_tbl[j].cur_state == current_state &&
+#             state_tbl[j].event == event)
+#       {
+#         tmp_event = event;
+#         event = EVENT_NONE;
+#         (*state_tbl[j].transition_func)(
+#           current_state,
+#           state_tbl[j].new_state,
+#           tmp_event);
+#         current_state = state_tbl[j].new_state;
+#         state_tbl_indx = j;
+#         break;
+#       }
+#     }
+#   }
+#   (*state_tbl[state_tbl_indx].monitor_func)(current_state);
+# }
